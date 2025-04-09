255a1c30bc220ccfef650bbfe360275affb56f49
yousefbahutair <yousefnas06@gmail.com>
2025-04-09 23:51:39 +0400

    feat: add a list of movie images and title

--- src/script.js
+++ src/script.js
@@ -4,12 +4,37 @@ const fetchData = async (searchTerm) => {
             apikey:'fdb5cf6d',
             s: searchTerm,
         }
+    });
+    if(res.data.Error){
+        return [];
     }
-)
-    console.log(res.data);
-}
-const onInput = e => {
- fetchData(e.target.value)
-}
+
+    return res.data.Search;
+};
+
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `<label><b>Search For a Movie</b></label>
+<input class="input"/>
+<div class="dropdown">
+<div class="dropdown-menu>
+  <div class="dropdown-content results></div>
+    </div>
+</div>
+`;
+
 const input = document.querySelector('input');
-input.addEventListener('input', debounce(onInput,1000))
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async e => {
+ const movies = await fetchData(e.target.value)
+ console.log(movies);
+ for(let movie of movies){
+    const div = document.createElement('div');
+    div.innerHTML = `<img src="${movie.Poster}"/>
+    <h1>${movie.Title}</h1>`
+    document.querySelector('#target').appendChild(div);
+
+ }
+}
+input.addEventListener('input', debounce(onInput,1000)) 

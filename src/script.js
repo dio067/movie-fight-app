6c21e680e81bf9aedb85dbf8ebaecfeef3097682
yousefbahutair <yousefnas06@gmail.com>
2025-04-10 08:16:12 +0400

    fix: handling empty responses

--- src/script.js
+++ src/script.js
@@ -17,24 +17,42 @@ root.innerHTML = `<label><b>Search For a Movie</b></label>
 <input class="input"/>
 <div class="dropdown">
 <div class="dropdown-menu>
-  <div class="dropdown-content results></div>
+  <div class="dropdown-content" id="results"></div>
     </div>
 </div>
 `;
 
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
+const resultsWrapper = document.querySelector('#results');
 
 const onInput = async e => {
  const movies = await fetchData(e.target.value)
+
+ if(movies.length){
+    dropdown.classList.remove('is-active')
+ }
+
  console.log(movies);
+ resultsWrapper.innerHTML = '';
+ dropdown.classList.add('is-active');
+
  for(let movie of movies){
-    const div = document.createElement('div');
-    div.innerHTML = `<img src="${movie.Poster}"/>
-    <h1>${movie.Title}</h1>`
-    document.querySelector('#target').appendChild(div);
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? "" : movie.Poster;
+
+    option.classList.add('dropdown-item')
+    option.innerHTML = `<img src="${imgSrc}"/>
+       ${movie.Title}
+    `;
+    console.log(option);
+    resultsWrapper.appendChild(option);
 
  }
 }
-input.addEventListener('input', debounce(onInput,1000)) 
+input.addEventListener('input', debounce(onInput,1000))
+document.addEventListener('click', e => {
+    if(!root.contains(e.target)){
+        dropdown.classList.remove('is-active');
+    }
+})

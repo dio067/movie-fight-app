d62cef60fe9a5c4a0af52989388364335556f322
yousefbahutair <yousefnas06@gmail.com>
2025-04-10 14:32:52 +0400

    feat: extracting selection logic

---
+++ src/autocomplete.js
@@ -0,0 +1,52 @@
+const createAutoComplete = ({root, renderOption, onSelectOption, inputValue}) => {
+ root.innerHTML = `<label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content" id="results"></div>
+    </div>
+  </div>
+`;
+
+const input = root.querySelector('input');
+const dropdown = root.querySelector('.dropdown');
+const resultsWrapper = root.querySelector('#results');
+
+const onInput = async e => {
+ const movies = await fetchData(e.target.value)
+
+ if(!movies.length){
+    dropdown.classList.remove('is-active');
+    return;
+ }
+
+ console.log(movies);
+ resultsWrapper.innerHTML = '';
+ dropdown.classList.add('is-active');
+
+ for(let movie of movies){
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? "" : movie.Poster;
+
+    option.classList.add('dropdown-item')
+    option.innerHTML = `${renderOption(movie)}
+    `;
+
+    option.addEventListener('click', e => {
+        dropdown.classList.remove('is-active');
+        input.value = inputValue(movie);
+        onSelectOption(movie);
+    })
+
+    console.log(option);
+    resultsWrapper.appendChild(option);
+
+ }
+}
+input.addEventListener('input', debounce(onInput,1000))
+document.addEventListener('click', e => {
+    if(!root.contains(e.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
+}
--- src/script.js
+++ src/script.js
@@ -12,59 +12,25 @@ const fetchData = async (searchTerm) => {
     return res.data.Search;
 };
 
-const root = document.querySelector('.autocomplete')
-root.innerHTML = `<label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content" id="results"></div>
-    </div>
-  </div>
-`;
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
 
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('#results');
-
-const onInput = async e => {
- const movies = await fetchData(e.target.value)
-
- if(!movies.length){
-    dropdown.classList.remove('is-active');
-    return;
- }
-
- console.log(movies);
- resultsWrapper.innerHTML = '';
- dropdown.classList.add('is-active');
-
- for(let movie of movies){
-    const option = document.createElement('a');
+    renderOption : (movie) => {
     const imgSrc = movie.Poster === 'N/A' ? "" : movie.Poster;
-
-    option.classList.add('dropdown-item')
-    option.innerHTML = `<img src="${imgSrc}"/>
-       ${movie.Title}
+    return `
+    <img src="${imgSrc}"/>
+       ${movie.Title} (${movie.Year})
     `;
+    },
 
-    option.addEventListener('click', e => {
-        dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        movieSelected(movie); // helper function to request data for selected movie
+    onSelectOption : (movie) => {
+        movieSelected(movie);
+    },
 
-    })
-
-    console.log(option);
-    resultsWrapper.appendChild(option);
-
- }
-}
-input.addEventListener('input', debounce(onInput,1000))
-document.addEventListener('click', e => {
-    if(!root.contains(e.target)){
-        dropdown.classList.remove('is-active');
+    inputValue : (movie) => {
+        return movie.Title;
     }
-})
+});
 
 const movieSelected = async (movie) => {
   const res =  await axios.get('http://www.omdbapi.com/', {
@@ -89,5 +55,26 @@ const movieTemplate = (movieDetails) => {
          <h4>${movieDetails.Genre}</h4>
          <p>${movieDetails.Plot}</p>
       </div>
-    </article>`
+    </article>
+      <article>
+        <p class="title">${movieDetails.Awards}</p>
+        <p class="subtitle">Awards</p>
+      </article>
+      <article>
+        <p class="title">${movieDetails.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+      </article>
+      <article>
+        <p class="title">${movieDetails.Metascore}</p>
+        <p class="subtitle">Meta Score</p>
+      </article>
+      <article>
+        <p class="title">${movieDetails.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+      </article>
+      <article>
+        <p class="title">${movieDetails.imdbVotes}</p>
+        <p class="subtitle">IMDB votes</p>
+      </article>`
+    
 };
